0b28387e9c3728c50a63df0d78e026d43fcc384a
Log0ns <55673686+Log0ns@users.noreply.github.com>
2025-11-06 10:18:18 -0600

    Update ScripturePlan.tsx

--- src/ScripturePlan.tsx
+++ src/ScripturePlan.tsx
@@ -672,6 +672,9 @@ export default function ScriptureReader() {
                   )}
                 </div>
               </div>
+              );
+          })}
+        </div>
       
         {/* Always-visible Return button */}
         <div className="fixed bottom-6 right-6 z-50">
