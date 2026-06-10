2135fd2c7485ba49b2d44042fe221edaf49a4a39
Log0ns <55673686+Log0ns@users.noreply.github.com>
2026-06-10 13:17:01 -0500

    Add files via upload

---
+++ ScripturePlan/src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as ReadingTile } from './ReadingTile';
+export { default as PrayerTile } from './PrayerTile';
+export { default as SettingsModal } from './SettingsModal';
+export { default as GlobalSettingsModal } from './GlobalSettingsModal';
+export { default as CustomTileSettings } from './CustomTileSettings';
