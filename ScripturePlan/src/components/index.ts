ffd0feda9f3a78f4428b72875ea9ce77baa78a27
Log0ns <55673686+Log0ns@users.noreply.github.com>
2026-06-12 14:57:20 -0500

    Delete ScripturePlan directory

--- ScripturePlan/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ReadingTile } from './ReadingTile';
-export { default as PrayerTile } from './PrayerTile';
-export { default as SettingsModal } from './SettingsModal';
-export { default as GlobalSettingsModal } from './GlobalSettingsModal';
-export { default as CustomTileSettings } from './CustomTileSettings';
