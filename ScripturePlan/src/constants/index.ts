0bd0ee6f259ed99d83db8025889f98411d72e202
Log0ns <55673686+Log0ns@users.noreply.github.com>
2026-08-25 11:31:03 -0500

    Delete ScripturePlan directory

--- ScripturePlan/src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-export { BIBLE_BOOKS } from './bibleBooks';
-export { DEFAULT_ICONS, READING_PLANS } from './plans';
-export { getTimeOfDay, getBackgroundGradient, getTileStyle, getTileTextColor, getIconColor, getRingStyle, updateMetaThemeColor } from './theme';
-export { MEMORY_CHUNKS } from './memoryChunks';
-export type { MemoryChunk } from './memoryChunks';
